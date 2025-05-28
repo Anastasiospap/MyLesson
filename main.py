a9cdb652c7cb1bd44f0126600219ff3e07731fa6
Anastasiospap <anastasiospap1@outlook.com>
2025-05-28 22:40:30 +0300

    ektupwsh

--- main.py
+++ main.py
@@ -9,3 +9,41 @@ print(str(athroisma))
 
 
 print("geia sou kosma")
+
+
+
+
+
+a = 5
+b = 3
+sunolo = a + b
+print(sunolo)
+
+a = 10
+b = 2
+afairesi = 10-2
+print(afairesi)
+
+a = 4
+b = 6
+pollaplasiase = a * b
+print(pollaplasiase)
+
+a = 20
+b = 4
+diairesh = 20/4
+print(diairesh)
+
+print("Καλημέρα Κόσμε!")
+
+print("Η Python είναι διασκεδαστική!")
+
+
+onoma = "Μαρία"
+hlikia = 25
+stoixeia = "Η Μαρία είναι 25 ετών"
+print(stoixeia)
+
+
+request = 5
+print(float(request))
