4d958332fc290508f465d730f1d18e77ab471018
Anastasiospap <anastasiospap1@outlook.com>
2025-06-05 14:19:19 +0300

    Homework 5/6/25

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from selectors import SelectSelector
+
 x=5 #theto integer
 print("x") #prosoxi etsi to diavazei san x sketo kai oxi 5
 print(str(x)) #str(x) -> 5 ->"5"
@@ -47,3 +49,70 @@ print(stoixeia)
 
 request = 5
 print(float(request))
+
+
+
+# new exercise 5/6/25
+
+# exercise 1
+α = 15
+b = 4
+διαιρεση = α / b
+print(διαιρεση)
+
+
+# exercise 2
+
+χ = 3
+
+χ5 = χ * 3 * 3 * 3 * 3 * 3
+print(χ5)
+
+# exercise 3
+
+p = 7
+
+if p == 7:
+    print("Ο Αριθμός είναι Περιττός")
+elif p == 6:
+    print("Ο αριθμός είναι Άρτιος")
+
+
+
+# exercise 4
+ar1 = 105
+ar2 = 96
+
+if ar1 > ar2:
+    print(ar1)
+elif ar1 < ar2:
+    print(ar2)
+
+
+
+# exercise 5
+
+for number in range(10):
+    print("αριθμός",number)
+
+
+# exercise 6
+
+x = 5
+
+while x <=50:
+    print(x)
+
+    x +=5
+
+
+# exercise 7
+
+
+list = [2, 4, 6, 8, 10]
+print(sum(list))
+
+
+# exercise 8
+
+
