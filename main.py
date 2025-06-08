265ff34a995ba9eac9c2e779311f5c0c57b0c583
Anastasiospap <anastasiospap1@outlook.com>
2025-06-08 22:14:50 +0300

    Homework 8/6/25

--- main.py
+++ main.py
@@ -1,118 +1,256 @@
-from selectors import SelectSelector
+# from selectors import SelectSelector
+#
+# x=5 #theto integer
+# print("x") #prosoxi etsi to diavazei san x sketo kai oxi 5
+# print(str(x)) #str(x) -> 5 ->"5"
+#
+# y=4
+# x=5
+# athroisma = x + y
+# print(str(athroisma))
+#
+#
+# print("geia sou kosma")
+#
+#
+#
+#
+#
+# a = 5
+# b = 3
+# sunolo = a + b
+# print(sunolo)
+#
+# a = 10
+# b = 2
+# afairesi = 10-2
+# print(afairesi)
+#
+# a = 4
+# b = 6
+# pollaplasiase = a * b
+# print(pollaplasiase)
+#
+# a = 20
+# b = 4
+# diairesh = 20/4
+# print(diairesh)
+#
+# print("Καλημέρα Κόσμε!")
+#
+# print("Η Python είναι διασκεδαστική!")
+#
+#
+# onoma = "Μαρία"
+# hlikia = 25
+# stoixeia = "Η Μαρία είναι 25 ετών"
+# print(stoixeia)
+#
+#
+# request = 5
+# print(float(request))
+#
+#
+#
+# # new exercise 5/6/25
+#
+# # exercise 1
+# α = 15
+# b = 4
+# διαιρεση = α / b
+# print(διαιρεση)
+#
+#
+# # exercise 2
+#
+# χ = 3
+#
+# χ5 = χ * 3 * 3 * 3 * 3 * 3
+# print(χ5)
+#
+# # exercise 3
+#
+# p = 7
+#
+# if p == 7:
+#     print("Ο Αριθμός είναι Περιττός")
+# elif p == 6:
+#     print("Ο αριθμός είναι Άρτιος")
+#
+#
+#
+# # exercise 4
+# ar1 = 105
+# ar2 = 96
+#
+# if ar1 > ar2:
+#     print(ar1)
+# elif ar1 < ar2:
+#     print(ar2)
+#
+#
+#
+# # exercise 5
+#
+# for number in range(10):
+#     print("αριθμός",number)
+#
+#
+# # exercise 6
+#
+# x = 5
+#
+# while x <=50:
+#     print(x)
+#
+#     x +=5
+#
+#
+# # exercise 7
+#
+#
+# list = [2, 4, 6, 8, 10]
+# print(sum(list))
+#
+#
+# # exercise 8
+#
+#
+#
+# x = float(input("δωσε εναν αριθμο"))
+# if x % 2 == 0:            #γιατι χρησιμοποιω στην ευκλειδια διαιρεση το 2 επι κάτι σύν κατι
+#     print("ο αριθμος ειναι αρτιος")
+# elif x % 2 == 1:
+#     print("ο αριθμος ειναι περιττος")
+#
+#
+# x = float(input("αριμος 1"))
+# y = float(input("αριθμος 2"))
+# if x > y:
+#     print(str(x))
+# elif x < y:
+#     print(str(y))
+#
+#
+#
+#
+# a = str(input("δωσε ενα ονομα"))
+# print(a)
+# arithmos_gramaton = len(a)
+# print(str(arithmos_gramaton))
+# if len(a) > 8:
+#     print("Γεια σου")
+#
+#
+#
+#
+# # 6/6/2025
+#
+# # exercise 1
+#
+#
+# ar = int(input("δωσε εναν ακεραιο αριθμο"))
+# if ar >= 1:
+#     print(str("ο αριθμος είναι θετικος"))
+# elif ar == 0:
+#     print(str("ο αριθμός ειναι μηδεν "))
+# elif ar <= -1:
+#     print(str("ο αριθμος ειναι αρνητικος"))
+#
+#
+# # exercise 2
+#
+# ar1 = 5.3
+# ar2 = 9.9
+#
+# ar1 = float(input("εισαγετε τον αριθμο 1"))
+# ar2 = float(input("εισαγετε τον αριθμο 2"))
+# if ar1 == ar2:
+#     print(str("οι αριθμοι είναι ισοι"))
+# elif ar1 < ar2:
+#     print(str("ο αριθμός 2 είναι μεγαλυτερος απο τον αριθμο 1 "))
+# elif ar1 > ar2:
+#     print(str("ο αριθμος 1 ειναι μεγαλυτερος απο τον αριθμο 2"))
+#
+# # exercise 3
+#
+# vathmos = 95
+#
+# vathmos = int(input("εισάγετε τον βαθμό σας"))
+#
+# if vathmos >= 95:
+#     print(str("ο βαθμός σας ειναι Αριστος"))
+#
+# elif vathmos >= 85 and vathmos < 95:
+#     print(str("o βαθμός σας ειναι Πολύ καλος"))
+#
+# elif vathmos >= 60 and vathmos < 85:
+#     print(str("ο βαθμός σας ειναι Καλος"))
+#
+# elif vathmos <= 60:
+#     print(str("ο βαθμος σας ειναι Ανεπαρκής"))
+#
+#
+# # exercise 4
+#
+# hlikia = int(input("εισάγετε την ηλικία σας"))
+# if hlikia >= 18:
+#     print(str("ο χρηστης ειναι ενηλικας"))
+# elif hlikia <= 18:
+#     print(str("ο χρηστης ειναι ανηλικος"))
 
-x=5 #theto integer
-print("x") #prosoxi etsi to diavazei san x sketo kai oxi 5
-print(str(x)) #str(x) -> 5 ->"5"
 
-y=4
-x=5
-athroisma = x + y
-print(str(athroisma))
+# # exercise 5
+# arithmos = float(input("γραψε εναν αριθμο "))
+# if arithmos % 3 == 0 and arithmos % 5 == 0:
+#     print(str("lucky number"))
+# else:
+#     print(str("ακυρος αριθμος"))
 
-
-print("geia sou kosma")
-
-
-
-
-
-a = 5
-b = 3
-sunolo = a + b
-print(sunolo)
-
-a = 10
-b = 2
-afairesi = 10-2
-print(afairesi)
-
-a = 4
-b = 6
-pollaplasiase = a * b
-print(pollaplasiase)
-
-a = 20
-b = 4
-diairesh = 20/4
-print(diairesh)
-
-print("Καλημέρα Κόσμε!")
-
-print("Η Python είναι διασκεδαστική!")
-
-
-onoma = "Μαρία"
-hlikia = 25
-stoixeia = "Η Μαρία είναι 25 ετών"
-print(stoixeia)
-
-
-request = 5
-print(float(request))
+# # exercise 6
+#
+# etos = int(input("γραψε ενα ετος "))
+# if etos % 4 == 0:
+#     if etos % 100 != 0:
+#         print(str("ειναι δισεκτο ετος"))
+#     elif etos % 100 == 0:
+#         print(str("το ετος δεν ειναι δισεκτο"))
+# elif etos % 4 != 0:
+#     print(str("το ετος δεν ειναι δισεκτο"))
 
 
 
-# new exercise 5/6/25
-
-# exercise 1
-α = 15
-b = 4
-διαιρεση = α / b
-print(διαιρεση)
-
-
-# exercise 2
-
-χ = 3
-
-χ5 = χ * 3 * 3 * 3 * 3 * 3
-print(χ5)
-
-# exercise 3
-
-p = 7
-
-if p == 7:
-    print("Ο Αριθμός είναι Περιττός")
-elif p == 6:
-    print("Ο αριθμός είναι Άρτιος")
 
 
 
-# exercise 4
-ar1 = 105
-ar2 = 96
 
-if ar1 > ar2:
-    print(ar1)
-elif ar1 < ar2:
-    print(ar2)
-
-
-
-# exercise 5
-
-for number in range(10):
-    print("αριθμός",number)
+#
+# # exercise 7
+# kodikos =str(input("δωσε κωδικο"))
+# if kodikos == "python123":
+#     print("σωστος κωδικος")
+# elif kodikos !="python123":
+#     print("λαθος κωδικος")
+#
 
 
-# exercise 6
 
-x = 5
+# # exercise 8
+#
+# ar1 = float(input("δωσε τον πρωτο αριθμο"))
+# ar2 = float(input("δωσε τον δευτερο αριθμο"))
+# ar3 = float(input("δωσε τον τριτο αριθμο"))
+# if ar1 > ar2:
+#     if ar1 > ar3:
+#         print(str("ο αριθμος 1 ειναι ο μεγαλυτερος"))
+#     elif ar3 > ar1:
+#         print(str("ο αριθμος 3 ειναι ο μεγαλυτερος"))
+# elif ar2 > ar1:
+#     if ar2 > ar3:
+#         print(str("ο αρθιμος 2 ειναι ο μεγαλυτερος"))
+#     elif ar3 > ar2:
+#         print(str("ο αριθμος 3 ειναι μεγαλυτερος"))
 
-while x <=50:
-    print(x)
 
-    x +=5
-
-
-# exercise 7
-
-
-list = [2, 4, 6, 8, 10]
-print(sum(list))
-
-
-# exercise 8
 
 
